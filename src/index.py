6dc063e14f69a7c62486ffc31f384fb94678f3bf
ymgorbunova <gorbunova_y_m@mail.ru>
2023-03-26 20:25:39 +0300

    test file modified

--- src/index.py
+++ src/index.py
@@ -1,6 +1,6 @@
 # файл начинается с комментария
 
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Response, Cookie
 from fastapi.responses import RedirectResponse, FileResponse, HTMLResponse
 from src.variables import journal, cryptogen, last_test_page_num
 
@@ -32,3 +32,18 @@ def root():
 def read_root():
     html_content = "<h2>Hello everyone METANIT.COM!</h2>"
     return HTMLResponse(content=html_content)
+
+
+@app.get("/cookie")
+def cookie_def(response: Response):
+    num = str(cryptogen.randrange(10 ** 29, 10 ** 30))
+    response.set_cookie(key="sessid", value=num)
+    return {"message": "куки установлены"}
+
+
+@app.get("/see_cookie")
+def cookie_get(sessid: str | None = Cookie(default=None)):
+    if sessid == None:
+        return {"message": "Это ваш первый визит на сайт"}
+    else:
+        return {"message": f"Ваш последний визит: {sessid}"}
